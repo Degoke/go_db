3269fdeb5fc6e1366eea6246e8796fef1c0f8055
Degoke <degokeadewoye@gmail.com>
2024-02-24 07:15:03 +0100

    kv store

--- btree.go
+++ btree.go
@@ -5,6 +5,40 @@ import (
 	"encoding/binary"
 )
 
+// node format
+// | type | nkeys |    pointers    |    offsets    | key-values
+// | 2B   | 2B    |   nkeys * 8B   |   nkeys * 2B  | ...
+
+// key-value format
+// | klen | vlen | key | val |
+// |  2B  |  2B  | ... | ... |
+
+const HEADER = 4
+
+const BTREE_PAGE_SIZE = 4096
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+func assert(cond bool) {
+	if !cond {
+    panic("assertion failure")
+  }
+}
+
+func init() {
+  node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+  assert(node1max <= BTREE_PAGE_SIZE)
+}
+
+const (
+  BNODE_NODE = 1 //internal nodes without values
+  BNODE_LEAF = 2 // leaf nodes with values
+)
+
+type BNode struct {
+  data []byte // can be dumped to disk
+}
+
 type BTree struct {
   // pointer (a non zero page number)
   root uint64
@@ -14,6 +48,79 @@ type BTree struct {
   del func(uint64)       // deallocate a page
 }
 
+//header
+func (node BNode) btype() uint16 {
+  return binary.LittleEndian.Uint16(node.data)
+}
+
+func (node BNode) nkeys() uint16 {
+  return binary.LittleEndian.Uint16(node.data[2:4])
+}
+
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+  binary.LittleEndian.PutUint16(node.data[0:2], btype)
+  binary.LittleEndian.PutUint16(node.data[2:4], nkeys)
+}
+
+// pointers
+func (node BNode) getPtr(idx uint16) uint64 {
+  assert(idx < node.nkeys())
+  pos := HEADER + 8*idx
+  return binary.LittleEndian.Uint64(node.data[pos:])
+}
+
+func (node BNode) setPtr(idx uint16, val uint64) {
+  assert(idx < node.nkeys())
+  pos := HEADER + 8*idx
+  binary.LittleEndian.PutUint64(node.data[pos:], val)
+}
+
+//offset list
+func offsetPos(node BNode, idx uint16) uint16 {
+  assert(1 <= idx && idx <= node.nkeys())
+  return HEADER + 8*node.nkeys() + 2*(idx-1)
+}
+
+func (node BNode) getOffset(idx uint16) uint16 {
+  if idx == 0 {
+    return 0
+  }
+  return binary.LittleEndian.Uint16(node.data[offsetPos(node, idx):])
+}
+
+func (node BNode) setOffset(idx uint16, offset uint16) {
+  binary.LittleEndian.PutUint16(node.data[offsetPos(node, idx):], offset)
+}
+
+// key-values
+func (node BNode) kvPos(idx uint16) uint16 {
+  assert(idx <= node.nkeys())
+  return HEADER + 8*node.nkeys() + 2*node.nkeys() + node.getOffset(idx)
+}
+
+func (node BNode) getKey(idx uint16) []byte {
+  assert(idx < node.nkeys())
+  pos := node.kvPos(idx)
+  klen := binary.LittleEndian.Uint16(node.data[pos:])
+  return node.data[pos+4:][:klen]
+}
+
+func (node BNode) getVal(idx uint16) []byte {
+  assert(idx < node.nkeys())
+
+  pos := node.kvPos(idx)
+  klen := binary.LittleEndian.Uint16(node.data[pos+0:])
+  vlen := binary.LittleEndian.Uint16(node.data[pos+2:])
+
+  return node.data[pos+4+klen:][:vlen]
+}
+
+// node size in bytes
+func (node BNode) nbytes() uint16 {
+  return node.kvPos(node.nkeys())
+}
+
+// returns the first kid node whose range intersects the key. (kid[i] <= key)
 func nodeLookupLe(node BNode, key []byte) uint16 {
   nkeys := node.nkeys()
   found := uint16(0)
@@ -84,6 +191,100 @@ func nodeAppendKv(new BNode, idx uint16, ptr uint64, key []byte, val []byte) {
   new.setOffset(idx + 1, new.getOffset(idx + 4 + uint16((len(key) + len(val)))))
 }
 
+// replace a link with the same key
+func nodeReplaceKid1ptr(new BNode, old BNode, idx uint16, ptr uint64) {
+	copy(new.data, old.data[:old.nbytes()])
+	new.setPtr(idx, ptr) // only the pointer is changed
+}
+
+//replace a link with multiple links
+func nodeReplaceKidN(
+  tree *BTree, new BNode, old BNode,
+  idx uint16, kids ...BNode,
+) {
+  inc := uint16(len(kids))
+
+  new.setHeader(BNODE_NODE, old.nkeys()+inc-1)
+  nodeAppendRange(new, old, 0, 0, idx)
+
+  for i, node := range kids {
+    nodeAppendKv(new, idx+uint16(i), tree.new(node), node.getKey(0), nil)
+  }
+
+  nodeAppendRange(new, old, idx+inc, idx+1, old.nkeys()-(idx+1))
+}
+
+// replace 2 adjacent links with 1
+func nodeReplace2Kid(
+	new BNode, old BNode, idx uint16,
+	ptr uint64, key []byte,
+) {
+	new.setHeader(BNODE_NODE, old.nkeys()-1)
+	nodeAppendRange(new, old, 0, 0, idx)
+	nodeAppendKv(new, idx, ptr, key, nil)
+	nodeAppendRange(new, old, idx+1, idx+2, old.nkeys()-(idx+2))
+}
+
+// split a bigger-than-allowed node into two.
+// the second node always fits on a page.
+func nodeSplit2(left BNode, right BNode, old BNode) {
+	assert(old.nkeys() >= 2)
+
+	// the initial guess
+	nleft := old.nkeys() / 2
+
+	// try to fit the left half
+	left_bytes := func() uint16 {
+		return HEADER + 8*nleft + 2*nleft + old.getOffset(nleft)
+	}
+	for left_bytes() > BTREE_PAGE_SIZE {
+		nleft--
+	}
+	assert(nleft >= 1)
+
+	// try to fit the right half
+	right_bytes := func() uint16 {
+		return old.nbytes() - left_bytes() + HEADER
+	}
+	for right_bytes() > BTREE_PAGE_SIZE {
+		nleft++
+	}
+	assert(nleft < old.nkeys())
+	nright := old.nkeys() - nleft
+
+	left.setHeader(old.btype(), nleft)
+	right.setHeader(old.btype(), nright)
+	nodeAppendRange(left, old, 0, 0, nleft)
+	nodeAppendRange(right, old, 0, nleft, nright)
+	// the left half may be still too big
+	assert(right.nbytes() <= BTREE_PAGE_SIZE)
+}
+
+// split a node if its too big, the results are 1-3 nodes
+func nodeSplit3(old BNode) (uint16, [3]BNode) {
+  if old.nbytes() <= BTREE_PAGE_SIZE {
+    old.data = old.data[:BTREE_PAGE_SIZE]
+    return 1, [3]BNode{old}
+  }
+
+  left := BNode{make([]byte, 2*BTREE_PAGE_SIZE)}
+  right := BNode{make([]byte, BTREE_PAGE_SIZE)}
+
+  nodeSplit2(left, right, old)
+
+  if left.nbytes() <= BTREE_PAGE_SIZE {
+    left.data = left.data[:BTREE_PAGE_SIZE]
+    return 2, [3]BNode{left, right}
+  }
+
+  // the lefty node is still too large
+  leftleft := BNode{make([]byte, BTREE_PAGE_SIZE)}
+  middle := BNode{make([]byte, BTREE_PAGE_SIZE)}
+  nodeSplit2(left, middle, left)
+  assert(leftleft.nbytes() <= BTREE_PAGE_SIZE)
+  return 3, [3]BNode{leftleft, middle, right}
+}
+
 //. add a new key to a leaf node
 func leafInsert(
   new BNode, old BNode, idx uint16,
@@ -93,6 +294,17 @@ func leafInsert(
   nodeAppendRange(new, old, 0, 0, idx)
   nodeAppendKv(new, idx, 0, key, val)
   nodeAppendRange(new, old, idx+1, idx, old.nkeys()-idx)
+}
+
+// update an existing key from a leaf node
+func leafUpdate(
+	new BNode, old BNode, idx uint16,
+	key []byte, val []byte,
+) {
+	new.setHeader(BNODE_LEAF, old.nkeys())
+	nodeAppendRange(new, old, 0, 0, idx)
+	nodeAppendKv(new, idx, 0, key, val)
+	nodeAppendRange(new, old, idx+1, idx+1, old.nkeys()-(idx+1))
 }
 
 // insert a kv into a node, the result might be split into 2 nodes
@@ -143,54 +355,6 @@ func nodeInsert(
   nsplit, splitted := nodeSplit3(knode)
   // update the kid links
   nodeReplaceKidN(tree, new, node, idx, splitted[:nsplit]...)
-}
-
-// ,split a bigger than allowed node into 2
-// the second node always fits on a page
-func nodeSplit2(left BNode, right BNode, old BNode) {
-  //
-}
-
-// split a node if its too big, the results are 1-3 nodes
-func nodeSplit3(old BNode) (uint16, [3]BNode) {
-  if old.nbytes() <= BTREE_PAGE_SIZE {
-    old.data = old.data[:BTREE_PAGE_SIZE]
-    return 1, [3]BNode{old}
-  }
-
-  left := BNode{make([]byte, 2*BTREE_PAGE_SIZE)}
-  right := BNode{make([]byte, BTREE_PAGE_SIZE)}
-
-  nodeSplit2(left, right, old)
-
-  if left.nbytes() <= BTREE_PAGE_SIZE {
-    left.data = left.data[:BTREE_PAGE_SIZE]
-    return 2, [3]BNode{left, right}
-  }
-
-  // the lefty node is still too large
-  leftleft := BNode{make([]byte, BTREE_PAGE_SIZE)}
-  middle := BNode{make([]byte, BTREE_PAGE_SIZE)}
-  nodeSplit2(left, middle, left)
-  assert(leftleft.nbytes() <= BTREE_PAGE_SIZE)
-  return 3, [3]BNode{leftleft, middle, right}
-}
-
-//replace a link with multiple links
-func nodeReplaceKidN(
-  tree *BTree, new BNode, old BNode,
-  idx uint16, kids ...BNode,
-) {
-  inc := uint16(len(kids))
-
-  new.setHeader(BNODE_NODE, old.nkeys()+inc-1)
-  nodeAppendRange(new, old, 0, 0, idx)
-
-  for i, node := range kids {
-    nodeAppendKv(new, idx+uint16(i), tree.new(node), node.getKey(0), nil)
-  }
-
-  nodeAppendRange(new, old, idx+inc, idx+1, old.nkeys()-(idx+1))
 }
 
 //remove a key from a leaf node
@@ -363,4 +527,27 @@ func (tree *BTree) Insert(key []byte, val []byte) {
   } else {
     tree.root = tree.new(splitted[0])
   }
+}
+
+func nodeGetKey(tree *BTree, node BNode, key []byte) ([]byte, bool) {
+  idx := nodeLookupLe(node, key)
+  switch node.btype() {
+  case BNODE_LEAF:
+    if bytes.Equal(key, node.getKey(idx)) {
+      return node.getVal(idx), true
+    } else {
+      return nil, false
+    }
+  case BNODE_NODE:
+    return nodeGetKey(tree, tree.get(node.getPtr(idx)), key)
+  default:
+    panic("bad nodeQ")
+  }
+}
+
+func (tree *BTree) Get(key []byte) ([]byte, bool) {
+  if tree.root == 0 {
+    return nil, false
+  }
+  return nodeGetKey(tree, tree.get(tree.root), key)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Degoke/godb
+
+go 1.18
